8fe9338c6e39b785f1f6f3513ab1374d8b7f76bc
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-05-04 01:27:23 +0200

    Initial commit

---
+++ restore.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+# restore.py:
+#       A simple script to serialize database into yaml, or json
+#
+# Copyright (c)
+# Andres Eloy Rivera Garcia
+#
+# SPDX-License-Identifier: MIT
+#
+import pickle
+import yaml
+import json
+from re import match
+
+iname   = 'anime.list' # Database name
+oname   = 'anime.yaml' # Use .json for json serialization, .yaml for yaml
+
+with open(iname, 'rb') as file: 
+    data = pickle.load(file, encoding='bytes')
+
+with open(oname, 'w') as file:
+    if   match('.*(.json)', oname): json.dump(data, file, sort_keys=True, default=str, indent=4)
+    elif match('.*(.yaml)', oname): yaml.dump(data, file)
+    else: print('Unrecognized file extension:', oname)

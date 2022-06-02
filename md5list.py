368166b122044374b406806f91551739418d27b6
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-06-02 09:16:25 +0200

    Add minor changes

--- md5list.py
+++ md5list.py
@@ -18,18 +18,18 @@ from subprocess import check_output as co
 
 import yaml
 
-FILENAME = '.md5list.yaml'
-MIMETYPE = 'text/x-python' # restrict to only python files
+FILENAME = ".md5list.yaml"
+MIMETYPE = "text/x-python"  # restrict to only python files
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     md5list = []
     for file in os.listdir():
-        if re.search(r'.*: ' + MIMETYPE, co(['mimetype', file]).decode()) is None:
+        if re.search(r".*: " + MIMETYPE, co(["mimetype", file]).decode()) is None:
             continue
-        md5 = re.search(r'\S+', co(['md5sum', file]).decode())[0]
+        md5 = re.search(r"\S+", co(["md5sum", file]).decode())[0]
         mtime = os.path.getmtime(file)
 
-        md5list.append({'name': file, 'md5': md5, 'mtime': mtime})
+        md5list.append({"name": file, "md5": md5, "mtime": mtime})
 
-    with open(FILENAME, 'w', encoding='utf-8') as file:
+    with open(FILENAME, "w", encoding="utf-8") as file:
         yaml.dump(md5list, file)
